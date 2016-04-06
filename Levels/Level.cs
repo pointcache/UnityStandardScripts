56da0050b03c22d7325054a25176278a51797e0b
pointcache <antonov.3d@gmail.com>
2016-04-06 14:09:53 -0500

    Reintergrated Levels, should be stable now

---
+++ Levels/Level.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace USTD.Levels
+{
+    /// <summary>
+    /// This class describes level object
+    /// </summary>
+    public class Level : ScriptableObject
+    {
+        //add your own customization, for example "utility level", "race track", "village/town"
+        public LevelType levelType;
+        public enum LevelType
+        {
+            game
+        }
+
+        public string Name; //optional
+        public string Description; //optional
+        /// <summary>
+        /// Mandatory starts after Assets/
+        /// </summary>
+        public string FolderPath;
+
+        /// <summary>
+        /// Indicates if level was fully loaded
+        /// </summary>
+        [HideInInspector]
+        public bool LevelLoaded;
+        /// <summary>
+        /// Internal cached scene that will become active
+        /// </summary>
+        [HideInInspector]
+        public Scene LevelActiveScene;
+
+        /// <summary>
+        /// Public field to reference your desired scene that will become the active scene of the level
+        /// </summary>
+        public Object ActiveScene;
+
+        /// <summary>
+        /// List of paths to the scenes in our folder
+        /// </summary>
+        public List<string> scenesInFolderPaths;
+
+        /// <summary>
+        /// Collect all necessary data
+        /// </summary>
+        /// <returns></returns>
+        public List<string> Cache()
+        {
+            scenesInFolderPaths = new List<string>();
+            var scenes = Directory.GetFiles(Application.dataPath + "/" + FolderPath);
+            foreach (var s in scenes)
+            {
+                if (s.Contains(".asset"))
+                {
+                    continue;
+                }
+                string d = s.Replace(Application.dataPath + "/", "");
+                if (d.Contains("meta"))
+                    continue;
+
+                d = d.Replace('\\', '/');
+                scenesInFolderPaths.Add(d);
+            }
+            return scenesInFolderPaths;
+        }
+
+        /// <summary>
+        /// This is what actually performs the level loading from within the game, it doesnt cover Activation of the active scene,
+        /// because it should be done with coroutine and coroutines work only with MonoBehaviour that is why we need level Activator
+        /// </summary>
+        public void LoadLevel()
+        {
+            int c = scenesInFolderPaths.Count;
+            for (int i = 0; i < c; i++)
+            {
+                string scene = scenesInFolderPaths[i];
+                SceneManager.LoadScene(scene.Replace(".unity", ""), LoadSceneMode.Additive);
+
+                string[] arr_name = scene.Split('/');
+                string name = arr_name[arr_name.Length - 1].Replace(".unity", "");
+
+                if (ActiveScene.name == name)
+                {
+                    LevelActiveScene = SceneManager.GetSceneByPath("Assets/" + scene);
+                }
+            }
+            LevelLoaded = true;
+        }
+    }
+}
---
+++ Levels/LevelActivator.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using USTD.Levels;
+using USTD;
+using UnityEngine.SceneManagement;
+namespace USTD.Levels
+{
+    /// <summary>
+    /// Special Component that handles level loading
+    /// </summary>
+    public class LevelActivator : MonoBehaviour
+    {
+        [SerializeField]
+        Level level;
+        [SerializeField]
+        USTDEditorPrefs prefs;
+        [SerializeField]
+        bool editorMode;
+
+        /// <summary>
+        /// For launching from EDITOR
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="_prefs">This is done to pass editor preferences from editor</param>
+        /// <returns></returns>
+        public static LevelActivator New(Level level, USTDEditorPrefs _prefs)
+        {
+            GameObject go = new GameObject("Level Activator");
+            LevelActivator activator = go.AddComponent<LevelActivator>();
+            activator.level = level;
+            activator.prefs = _prefs;
+            //indicate that we launched from the editor
+            activator.editorMode = true;
+            return activator;
+        }
+
+        /// <summary>
+        /// Use this to load level in game. All necessary clean up operation prior to loading you have to manage yourself.
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        public static LevelActivator New(Level level)
+        {
+            GameObject go = new GameObject("Level Activator");
+            LevelActivator activator = go.AddComponent<LevelActivator>();
+            activator.level = level;
+            return activator;
+        }
+
+        // Not awake because we have to be sure that the scene were in is RUNNING
+        void Start()
+        {
+            level.LoadLevel();
+            if (editorMode)
+                StartCoroutine(TrackLoadingEditor());
+            else
+                StartCoroutine(TrackLoading());
+        }
+
+        IEnumerator TrackLoadingEditor()
+        {
+            int i = 0;
+            while (true)
+            {
+                //Wait 1 frame to activate the level ACTIVE scene
+                if (i == 1)
+                {
+                    if (level.LevelLoaded)
+                    {
+                        SceneManager.SetActiveScene(level.LevelActiveScene);
+                        prefs.EditorSceneLaunchMode = true;
+                        yield break;
+                    }
+                }
+                i++;
+                yield return null;
+            }
+        }
+
+        IEnumerator TrackLoading()
+        {
+            int i = 0;
+            while (true)
+            {
+                //Wait 1 frame to activate the level ACTIVE scene
+                if (i == 1)
+                {
+                    if (level.LevelLoaded)
+                    {
+                        SceneManager.SetActiveScene(level.LevelActiveScene);
+                        //self destruct
+                        Destroy(this.gameObject);
+                        yield break;
+                    }
+                }
+                i++;
+                yield return null;
+            }
+        }
+    }
+}
